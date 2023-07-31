cf847558856e5d01cc0f01b05176ab8c3b4ff2ac
Louis12391 <141030943+Louis12391@users.noreply.github.com>
2023-07-31 14:53:19 +0100

    Example of nesting if statements

---
+++ nesting.py
@@ -0,0 +1,12 @@
+num = int (input("Enter a two digit number"))
+if num > 10 :
+    print("Your number is larger than 10")
+    if num %2 == 0 : 
+        print("Your number is even")
+elif num < 10 :
+    print("Your number is below 10")
+    if num %2 == 1 :
+        print("Your number is odd")
+
+    
+        
