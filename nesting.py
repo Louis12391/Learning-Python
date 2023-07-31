01a4d602359d799c469bcd3b8d5568ecd8ebdccf
Louis12391 <141030943+Louis12391@users.noreply.github.com>
2023-07-31 14:54:31 +0100

    added comment about what modulo symbol is

--- nesting.py
+++ nesting.py
@@ -3,6 +3,7 @@ if num > 10 :
     print("Your number is larger than 10")
     if num %2 == 0 : 
         print("Your number is even")
+        #% means modulo
 elif num < 10 :
     print("Your number is below 10")
     if num %2 == 1 :
