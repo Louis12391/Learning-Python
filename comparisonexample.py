53246f8ec3f3c852831adb6817f9e8d9891862c9
Louis12391 <141030943+Louis12391@users.noreply.github.com>
2023-07-31 14:38:15 +0100

    Asking user 2 question and comparing them

---
+++ comparisonexample.py
@@ -0,0 +1,10 @@
+userinput = input ("What is your favourite food?")
+#user will be asked this question
+userinpat = input ("what do you eat for breakfast?")
+#user asked additional question
+if userinpat == userinput: 
+    print("You eat your favourite food for breakfast!")
+#add indentation after if statemtent(on next line)
+if userinpat != userinput:
+    print("You don`t eat your favourite food for breakfast")
+    
